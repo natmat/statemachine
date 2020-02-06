e236c7c81a2ac5be4dd1e283a7b357b325f0292b
Nathan <nmattick@gmail.com>
2020-02-06 22:23:22 +0000

    Adding an example, needs correcting.

--- statemachines.py
+++ statemachines.py
@@ -25,5 +25,22 @@ with open("EvacSM.cpp", "r") as f:
             print(result)
 
 
+ import re
+
+regex = r"\{(.*?)\}"
+
+test_str = ("Server_1 {\n"
+    "/directory1 /directory2\n\n"
+    "}\n"
+    "Server_2 {\n\n"
+    "/directory1\n\n"
+    "/directory2\n\n"
+    "}")
+
+matches = re.finditer(regex, test_str, re.MULTILINE | re.DOTALL)
+
+for matchNum, match in enumerate(matches):
+    for groupNum in range(0, len(match.groups())):
+        print (match.group(1))
 
 
